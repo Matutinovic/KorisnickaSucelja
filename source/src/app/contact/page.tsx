1b5f2d6d8ca9cc6c1bf08f23c177dc194593794f
cimamj <jmamic02@fesb.hr>
2024-11-21 12:03:53 +0100

    izgled nije funkcionirao pa iz novog foldera gdje izgled funkcionira

---
+++ source/src/app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Contact() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-10">
+      <h1 className="text-6xl font-extrabold tracking-tight">Contact</h1>
+    </main>
+  );
+}
---
+++ source/src/app/services/vjezbe/page.tsx
@@ -0,0 +1,7 @@
+export default function Vjezbe() {
+    return (
+      <main className="flex min-h-screen flex-col items-center p-10">
+        <h1 className="text-6xl font-extrabold tracking-tight">Vjezbe</h1>
+      </main>
+    );
+  }
