b8b05f599a626008bfeb620bf8bfdede6b2d1c99
cimamj <jmamic02@fesb.hr>
2025-01-13 18:29:48 +0100

    responzivnost 3

--- source/src/app/page.tsx
+++ source/src/app/page.tsx
@@ -21,77 +21,76 @@ const Page = () => (
       />
     </div>
 
-    <div className="absolute top-[18%] right-14 flex flex-col items-center space-y-4 z-10">
-  {/* Slika Bencher na vrhu */}
-  <Image
-    src="/bencher.png"
-    alt="Bencher"
-    width={400}
-    height={400}
-    className="rounded-lg max-w-[200px] sm:max-w-[300px] md:max-w-[400px]"
-  />
-
-  {/* Slike Vaga i Rast u istom redu, jedan do drugog */}
-  <div className="flex space-x-4 hidden sm:flex">
-    <div className="transform -translate-y-20">
+    <div className="absolute top-[18%] right-14 flex flex-col items-center space-y-4 z-0">
+      {/* Slika Bencher na vrhu */}
       <Image
-        src="/vaga.png"
-        alt="Vaga"
-        width={300}
-        height={300}
-        className="rounded-lg"
+        src="/bencher.png"
+        alt="Bencher"
+        width={400}
+        height={400}
+        className="rounded-lg max-w-[200px] sm:max-w-[300px] md:max-w-[400px]"
       />
+
+      {/* Slike Vaga i Rast u istom redu, jedan do drugog */}
+      <div className="flex space-x-4 hidden sm:flex">
+        <div className="transform -translate-y-20">
+          <Image
+            src="/vaga.png"
+            alt="Vaga"
+            width={300}
+            height={300}
+            className="rounded-lg"
+          />
+        </div>
+        <div className="transform -translate-y-20">
+          <Image
+            src="/rast.png"
+            alt="Rast"
+            width={200}
+            height={200}
+            className="rounded-lg"
+          />
+        </div>
+      </div>
     </div>
-    <div className="transform -translate-y-20">
-      <Image
-        src="/rast.png"
-        alt="Rast"
-        width={200}
-        height={200}
-        className="rounded-lg"
-      />
+
+    <div className="h-[800px]"></div>
+
+    {/* Novi */}
+    <div className="flex flex-col sm:flex-row justify-between items-center mt-20 px-5 sm:px-10">
+      {/* Slike */}
+      <div className="flex-shrink-0 mb-6 sm:mb-0 sm:mr-10">
+        <Image
+          src="/benc.png"
+          alt="Benc"
+          width={400}
+          height={300}
+          className="rounded-lg"
+        />
+      </div>
+
+      {/* Natuknice s desne strane */}
+      <BulletPoints />
     </div>
-  </div>
-</div>
 
-<div className="h-[800px]"></div>
+    <div className="h-[100px]"></div>
+    <div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mt-20 px-5 sm:px-10">
+      {/* BulletPoints2 */}
+      <BulletPoints2 />
 
-{/* Novi */}
-<div className="flex flex-col sm:flex-row justify-between items-center mt-20 px-5 sm:px-10">
-  {/* Slike */}
-  <div className="flex-shrink-0 mb-6 sm:mb-0 sm:mr-10">
-    <Image
-      src="/benc.png"
-      alt="Benc"
-      width={400}
-      height={300}
-      className="rounded-lg"
-    />
-  </div>
+      {/* Slika */}
+      <div className="flex-shrink-0 sm:mr-10 z-10 mt-6 sm:mt-0">
+        <Image
+          src="/dijagram.png"
+          alt="Benc"
+          width={400}
+          height={300}
+          className="rounded-lg"
+        />
+      </div>
+    </div>
 
-  {/* Natuknice s desne strane */}
-  <BulletPoints />
-</div>
-
-<div className="h-[100px]"></div>
-<div className="flex flex-col sm:flex-row justify-center sm:justify-between items-center mt-20 px-5 sm:px-10">
-  {/* BulletPoints2 */}
-  <BulletPoints2 />
-
-  {/* Slika */}
-  <div className="flex-shrink-0 sm:mr-10 z-10 mt-6 sm:mt-0">
-    <Image
-      src="/dijagram.png"
-      alt="Benc"
-      width={400}
-      height={300}
-      className="rounded-lg"
-    />
-  </div>
-</div>
-
-    
-    <Footer /> 
+    <Footer />
   </div>
 );
 
--- source/src/components/ButtonGroup.tsx
+++ source/src/components/ButtonGroup.tsx
@@ -1,10 +1,10 @@
 "use client";
 const ButtonGroup = () => (
-  <div className="absolute left-[5%] top-[72%] sm:left-[10%] sm:top-[68%] flex space-x-4 sm:space-x-10 z-20">
-    <button className="w-48 px-6 py-3 text-white bg-black text-xl font-semibold rounded-lg hover:bg-gray-800 transition-all duration-300">
+  <div className="absolute left-[5%] top-[78%] sm:left-[10%] sm:top-[72%] flex space-x-4 sm:space-x-8 z-20">
+    <button className="w-40 px-5 py-2 text-white bg-black text-lg font-semibold rounded-lg hover:bg-gray-800 transition-all duration-300">
       SIGN IN
     </button>
-    <button className="w-48 px-6 py-3 text-black border-2 border-black text-xl font-semibold rounded-lg hover:bg-black hover:text-white transition-all duration-300">
+    <button className="w-40 px-5 py-2 text-black border-2 border-black text-lg font-semibold rounded-lg hover:bg-black hover:text-white transition-all duration-300">
       FREE TRIAL
     </button>
   </div>
